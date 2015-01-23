7d61223672194e0e8d44097672b3acac8b915467
Brendan Ragan <lordmortis@gmail.com>
2015-01-23 23:07:21 +0800

    Health Bars!

---
+++ Assets/HealthActor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthActor : MonoBehaviour {
+	// Name to display for this character
+	public string characterName;
+
+	// Health for character
+	public float health;
+
+	public void Awake() {
+		nameObject = transform.Find ("CharacterName").gameObject.GetComponent<Text> ();
+		if (nameObject == null)
+			Debug.Log ("OH NO. COULDNT FIND NAME");
+		
+		healthBarObject = transform.Find ("Bar").gameObject.GetComponent<Image> ();
+		if (healthBarObject == null) {
+			Debug.Log ("OH NO. COULDNT FIND HEALTH");
+		} else {
+			healthBarInitial = healthBarObject.rectTransform.sizeDelta;
+		}
+		
+		SetName(characterName);
+		SetHealth(health);
+	}
+
+	// Use this for initialization
+	public void Start () {
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+
+	public void SetName(string value) {
+		nameObject.text = value;
+	}
+
+	private void SetHealth(float value) {
+		if (value < 0.0f) {
+			Debug.Log ("Warning: Health value " + value + " Below 0.0f");
+			value = 0.0f;
+		} else if (value > 1.0f) {
+			Debug.Log ("Warning: Health value " + value + " Above 1.0f");
+			value = 1.0f;
+		}
+	
+		healthBarObject.rectTransform.sizeDelta = new Vector2 (healthBarInitial.x * value, healthBarInitial.y);
+	}
+
+	private Text nameObject;
+	private Image healthBarObject;
+	private Vector2 healthBarInitial;
+}
---
+++ Assets/temp.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class temp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		healthbar = GameObject.Find ("HealthBar");
+		healthbar.SendMessage ("SetName", "Joe Bloggs", SendMessageOptions.RequireReceiver);
+		healthbar.SendMessage ("SetHealth", health, SendMessageOptions.RequireReceiver);
+		healthbar2 = GameObject.Find ("Health Bar");
+		healthbar2.SendMessage ("SetName", "Globs", SendMessageOptions.RequireReceiver);
+		healthbar2.SendMessage ("SetHealth", health2, SendMessageOptions.RequireReceiver);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - lastTime) > 0.2f) {
+			health += 0.01f;
+			if (health > 1.0f)
+				health = 0.1f;
+
+			healthbar.SendMessage ("SetHealth", health, SendMessageOptions.RequireReceiver);
+			lastTime = Time.time;
+		}
+		if ((Time.time - lastTime2) > 0.4f) {
+			health2 -= 0.01f;
+			if (health2 < 0.1f)
+				health2 = 1.0f;
+			
+			healthbar2.SendMessage ("SetHealth", health2, SendMessageOptions.RequireReceiver);
+			lastTime2 = Time.time;
+		}
+	}
+
+	private GameObject healthbar;
+	private GameObject healthbar2;
+	private float lastTime;
+	private float lastTime2;
+	private float health = 0.1f;
+	private float health2 = 1.0f;
+}
