5a59494f92fdbeef929dd99aacd84edeeb48469b
Jetha Chan <jethachan@gmail.com>
2015-01-23 23:33:18 +0800

    Simple player controller using InControl and billboard behavior for player sprites.

---
+++ Assets/src/behaviours/InputWrangler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class InputWrangler : MonoBehaviour {
+
+    int NumDevices = 0;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    NumDevices = InputManager.Devices.Count;
+
+        Debug.Log(string.Format("{0} devices.", NumDevices));
+	}
+}
