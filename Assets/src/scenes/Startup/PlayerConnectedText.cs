a800d43d5b55f377035ac71cd7bb77fcfffa6b2a
Brendan Ragan <lordmortis@gmail.com>
2015-01-24 11:50:17 +0800

    create startup scene, add code for detecting available controllers

---
+++ Assets/src/scenes/Startup/PlayerConnectedText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using InControl;
+
+public class PlayerConnectedText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UpdateText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (connectedDevices != InputManager.Devices.Count) {
+			UpdateText();
+		}
+	}
+
+	private void UpdateText() {
+		connectedDevices = InputManager.Devices.Count;
+		if (connectedDevices == 0) {
+			this.GetComponent<Text>().text = "No Controllers Found";
+		} else if (connectedDevices == 1) {
+			this.GetComponent<Text>().text = "1 Controller Found";
+		} else if (connectedDevices > 1) {
+			this.GetComponent<Text>().text = connectedDevices + " Controllers Found";
+		}
+	}
+
+	private int connectedDevices = 0;
+}
