4f6739329e540961812ead438535e77d21ff735f
doug <linderd@iinet.net.au>
2015-01-24 12:42:01 +0800

    Makes the character test set work again

--- Assets/src/behaviours/Billboard.cs
+++ Assets/src/behaviours/Billboard.cs
@@ -10,30 +10,34 @@ public class Billboard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
         float f = (Convert.ToSingle(bFlip) - 0.5f) * 2.0f;
         Vector3 lookOffset = Vector3.back * f;
 
-        lookTarget = gameObject.transform.parent.position + lookOffset;
+        // Allow this to work in the character test scene with no immediate parent
+        if (gameObject.transform.parent) {
 
-        this.transform.LookAt(lookTarget, Vector3.up);
+            lookTarget = gameObject.transform.parent.position + lookOffset;
 
-        const float debugDrawLength = 4.0f;
-        Debug.DrawLine(
-            transform.position,
-            transform.position + debugDrawLength * transform.forward.normalized,
-            Color.green
-        );
+            this.transform.LookAt(lookTarget, Vector3.up);
+
+            const float debugDrawLength = 4.0f;
+            Debug.DrawLine(
+                transform.position,
+                transform.position + debugDrawLength * transform.forward.normalized,
+                Color.green
+            );
+        }
 	}
 
     void FlipBillboard(bool flipped = true) {
         bFlip = flipped;
     }
 
-    
+
 }
