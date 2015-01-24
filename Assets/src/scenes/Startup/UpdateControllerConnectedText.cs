2cde1fde9ccc3787270488615e64697719fe4e02
Jetha Chan <jethachan@gmail.com>
2015-01-24 21:48:10 +0800

    Eyyyyyy. Made it possible to set gamestart timer from editor.

--- Assets/src/scenes/Startup/UpdateControllerConnectedText.cs
+++ Assets/src/scenes/Startup/UpdateControllerConnectedText.cs
@@ -7,6 +7,8 @@ public class UpdateControllerConnectedText : MonoBehaviour
 {
     public float updateClockInterval = 0.01f;
 
+    public float BrendansMagicNumber = 5.0f;
+
     public void OnNumControllersChanged(int connectedDevices)
     {
         SetControllerText(connectedDevices);
@@ -62,7 +64,7 @@ public class UpdateControllerConnectedText : MonoBehaviour
     {
 		if ((gc.NumberOfPlayers == InputManager.Devices.Count) && (gc.NumberOfPlayers > 0))
 		{
-			startTime = Time.time + 5.0f;
+			startTime = Time.time + BrendansMagicNumber;
 		}
 			else
 		{
