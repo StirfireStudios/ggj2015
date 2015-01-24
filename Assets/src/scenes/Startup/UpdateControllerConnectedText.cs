279303caaf5f0d3385e7fd21d3d4f72535eed2f5
Jetha Chan <jethachan@gmail.com>
2015-01-24 17:01:25 +0800

    Add check for input manager device count being zero; VS also hissed and spat at Brendan's formatting.

--- Assets/src/scenes/Startup/UpdateControllerConnectedText.cs
+++ Assets/src/scenes/Startup/UpdateControllerConnectedText.cs
@@ -3,64 +3,84 @@ using UnityEngine.UI;
 using System.Collections;
 using InControl;
 
-public class UpdateControllerConnectedText : MonoBehaviour {
-	public float updateClockInterval = 0.01f;
+public class UpdateControllerConnectedText : MonoBehaviour
+{
+    public float updateClockInterval = 0.01f;
 
-	public void OnNumControllersChanged(int connectedDevices) {
-		SetControllerText (connectedDevices);
-		CheckReadyState();
-	}
+    public void OnNumControllersChanged(int connectedDevices)
+    {
+        SetControllerText(connectedDevices);
+        CheckReadyState();
+    }
 
-	public void Update() {
-		if (startTime < 0.0f) {
-			return;
-		}
+    public void Update()
+    {
+        if (startTime < 0.0f)
+        {
+            return;
+        }
 
-		if (startTime < Time.time) {
-			Debug.Log ("START GAME");
-			startTime = -1.0f;
-		}
+        if (startTime < Time.time)
+        {
+            Debug.Log("START GAME");
+            startTime = -1.0f;
+        }
 
-		if (Time.time - lastTextUpdate > updateClockInterval) {
-			this.GetComponent<Text>().text = (startTime - Time.time) + " Seconds left";
-			lastTextUpdate = Time.time;
-		}
-	}
+        if (Time.time - lastTextUpdate > updateClockInterval)
+        {
+            this.GetComponent<Text>().text = (startTime - Time.time) + " Seconds left";
+            lastTextUpdate = Time.time;
+        }
+    }
 
-	public void SetCharacterPairing(System.Collections.Generic.Dictionary<string, object> data) {
-		InputDevice device = (InputDevice)data ["controller"];
-		string name = (string)data ["character"];
-		characterMapping [device] = name;
-		CheckReadyState ();
-	}
-	
-	public void RemoveCharacterForDevice(InputDevice device) {
-		if (characterMapping.ContainsKey (device)) {
-			characterMapping.Remove(device);
-		}
-		CheckReadyState ();
-	}
+    public void SetCharacterPairing(System.Collections.Generic.Dictionary<string, object> data)
+    {
+        InputDevice device = (InputDevice)data["controller"];
+        string name = (string)data["character"];
+        characterMapping[device] = name;
+        CheckReadyState();
+    }
 
-	private void CheckReadyState() {
-		if (characterMapping.Count == InputManager.Devices.Count) {
-			startTime = Time.time + 5.0f;
-		} else {
-			startTime = -1.0f;
-			SetControllerText(InputManager.Devices.Count);
-		}
-	}
+    public void RemoveCharacterForDevice(InputDevice device)
+    {
+        if (characterMapping.ContainsKey(device))
+        {
+            characterMapping.Remove(device);
+        }
+        CheckReadyState();
+    }
 
-	private void SetControllerText(int connectedDevices) {
-		if (connectedDevices == 0) {
-			this.GetComponent<Text>().text = "No Controllers Found";
-		} else if (connectedDevices == 1) {
-			this.GetComponent<Text>().text = "1 Controller Found";
-		} else if (connectedDevices > 1) {
-			this.GetComponent<Text>().text = connectedDevices + " Controllers Found";
-		}
-	}
+    private void CheckReadyState()
+    {
+        if (InputManager.Devices.Count > 0)
+            if (characterMapping.Count == InputManager.Devices.Count)
+            {
+                startTime = Time.time + 5.0f;
+            }
+            else
+            {
+                startTime = -1.0f;
+                SetControllerText(InputManager.Devices.Count);
+            }
+    }
 
-	private float startTime = -1.0f;
-	private float lastTextUpdate = -1.0f;
-	private System.Collections.Generic.Dictionary<InputDevice, string> characterMapping = new System.Collections.Generic.Dictionary<InputDevice, string>();	
+    private void SetControllerText(int connectedDevices)
+    {
+        if (connectedDevices == 0)
+        {
+            this.GetComponent<Text>().text = "No Controllers Found";
+        }
+        else if (connectedDevices == 1)
+        {
+            this.GetComponent<Text>().text = "1 Controller Found";
+        }
+        else if (connectedDevices > 1)
+        {
+            this.GetComponent<Text>().text = connectedDevices + " Controllers Found";
+        }
+    }
+
+    private float startTime = -1.0f;
+    private float lastTextUpdate = -1.0f;
+    private System.Collections.Generic.Dictionary<InputDevice, string> characterMapping = new System.Collections.Generic.Dictionary<InputDevice, string>();
 }
