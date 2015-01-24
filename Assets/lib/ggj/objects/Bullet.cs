66a53c5c0a7dacb5cb33264da37a6250712e0cc4
doug <linderd@iinet.net.au>
2015-01-24 15:46:10 +0800

    Shooting action for character

---
+++ Assets/lib/ggj/objects/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GGJ {
+
+	/** Behaviour for bullet types */
+	public class Bullet : MonoBehaviour {
+
+		/** Maximum lifetime this bullet will live for */
+		public float maxLife;
+
+		/** Actual life so far */
+		private float _life;
+
+		void Start () {
+			_life = 0f;
+		}
+
+		void Update () {
+			_life += Time.deltaTime;
+			if (_life > maxLife) {
+				N.Meta._(this).destroy();
+			}
+		}
+	}
+}
